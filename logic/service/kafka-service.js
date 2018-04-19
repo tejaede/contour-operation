8a0c7ecec331d41585fad1c114ef05e9bdaf56c5
Harold Thetiot <hthetiot@gmail.com>
2018-04-19 11:09:57 -0700

    implement kafka stack/tools/test/spec

--- logic/service/kafka-service.js
+++ logic/service/kafka-service.js
@@ -4,23 +4,29 @@ var HttpService = require("montage/data/service/http-service").HttpService,
 /*
 // Producer
 // TODO
-var kafka = require('kafka-node');
-var Producer = kafka.Producer;
-var KeyedMessage = kafka.KeyedMessage;
-var Client = kafka.Client;
+var topic = 'topic1';
+var partition = 0;
+var attributes = 0;
+
+var message = program.message;
+var keyedMessage = new KeyedMessage(program.key, message);
+
 var client = new Client('localhost:2181');
-var argv = require('optimist').argv;
-var topic = argv.topic || 'topic1';
-var p = argv.p || 0;
-var a = argv.a || 0;
-var producer = new Producer(client, { requireAcks: 1 });
+var producer = new Producer(client, { 
+  requireAcks: 1 
+});
 
 producer.on('ready', function () {
-  var message = 'a message';
-  var keyedMessage = new KeyedMessage('keyed', 'a keyed message');
-
   producer.send([
-    { topic: topic, partition: p, messages: [message, keyedMessage], attributes: a }
+    { 
+    topic: topic, 
+    partition: partition, 
+    messages: [
+      message, 
+      keyedMessage
+    ], 
+    attributes: attributes
+  }
   ], function (err, result) {
     console.log(err || result);
     process.exit();
@@ -33,17 +39,16 @@ producer.on('error', function (err) {
 
 // Consumer
 // TODO
-var kafka = require('kafka-node');
-var Consumer = kafka.Consumer;
-var Offset = kafka.Offset;
-var Client = kafka.Client;
-var argv = require('optimist').argv;
 var topic = argv.topic || 'topic1';
+var topics = [{ 
+  topic: topic, partition: 1 
+}, { 
+  topic: topic, 
+  partition: 0 
+}];
 
 var client = new Client('localhost:2181');
-var topics = [{ topic: topic, partition: 1 }, { topic: topic, partition: 0 }];
 var options = { autoCommit: false, fetchMaxWaitMs: 1000, fetchMaxBytes: 1024 * 1024 };
-
 var consumer = new Consumer(client, topics, options);
 var offset = new Offset(client);
 
@@ -53,6 +58,21 @@ consumer.on('message', function (message) {
 
 consumer.on('error', function (err) {
   console.log('error', err);
+});
+
+/*
+* If consumer get `offsetOutOfRange` event, fetch data from the smallest(oldest) offset
+*/
+consumer.on('offsetOutOfRange', function (topic) {
+  topic.maxNum = 2;
+  offset.fetch([topic], function (err, offsets) {
+    if (err) {
+      return console.error(err);
+    }
+    var min = Math.min.apply(null, offsets[topic.topic][topic.partition]);
+    consumer.setOffset(topic.topic, topic.partition, min);
+  });
+});e.log('error', err);
 });
 
 /*
---
+++ tools/kafka-consumer.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var kafka = require('kafka-node');
+var Consumer = kafka.Consumer;
+var Offset = kafka.Offset;
+var Client = kafka.Client;
+
+var program = require('commander');
+ 
+program
+  .version('0.1.0') // TODO use package versiob
+  .option('-f, --start [topic]', 'start consumer for given topic', 'topic1')
+  .option('-p, --partition [partition]', 'partition', '0')
+  .parse(process.argv);
+
+var topic = program.start;
+var partition = 0;
+
+var topics = [{ 
+	topic: topic, 
+	partition: 1 
+}, { 
+	topic: topic, 
+	partition: 0 
+}];
+
+var client = new Client('localhost:2181');
+var options = { autoCommit: false, fetchMaxWaitMs: 1000, fetchMaxBytes: 1024 * 1024 };
+var consumer = new Consumer(client, topics, options);
+var offset = new Offset(client);
+
+consumer.on('message', function (message) {
+  console.log(message);
+});
+
+consumer.on('error', function (err) {
+  console.log('error', err);
+  process.exit(1);
+});
+
+/*
+* If consumer get `offsetOutOfRange` event, fetch data from the smallest(oldest) offset
+*/
+consumer.on('offsetOutOfRange', function (topic) {
+  topic.maxNum = 2;
+  offset.fetch([topic], function (err, offsets) {
+    if (err) {
+      return console.error(err);
+    }
+    var min = Math.min.apply(null, offsets[topic.topic][topic.partition]);
+    consumer.setOffset(topic.topic, topic.partition, min);
+  });
+});
