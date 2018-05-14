b5ff2213806f1d17c3c16fcd2c5718c6cec3e0c1
Harold Thetiot <hthetiot@gmail.com>
2018-05-14 12:48:51 -0700

    merge fixes

--- test/configure.js
+++
@@ -1,8 +0,0 @@
-// Configure Test ENV
-chai.use(chaiHttp);
-// Export Global
-global = eval("this");
-global.chai = chai;
-global.should = chai.should();
-global.expect = chai.expect;
-global.io = io;
